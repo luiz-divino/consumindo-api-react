fd32385bc43dae0e2d9bfb2e19c0c1f7fd2753f5
luizdivino <nandodivinodev@gmail.com>
2026-03-30 20:21:49 -0300

    feat: criar componente Header com titulo do projeto

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className="bg-gray-800 text-white p-4 w-full flex items-center justify-center">
+            <h1 className="text-4xl font-bold">Nutrição</h1>
+        </header>
+    )
+}
