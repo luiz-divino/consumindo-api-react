0e8138b74df2c5e769f46e08d71c2dd061154fb4
luizdivino <nandodivinodev@gmail.com>
2026-03-30 18:24:51 -0300

    chore: adicionar tipagem de dados na api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,12 @@
-export const api = async() => {
+import { ApiResponse } from '../types/apiContract';
+
+export const api = async (): Promise<ApiResponse[]> => {
     try {
         const response = await fetch('https://sujeitoprogramador.com/rn-api/?api=posts');
-        const data = await response.json();
+        const data: ApiResponse[] = await response.json();
         return data;
     } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
     }
-}
+};
