d0c5c4556692e9addd8dfa41e8b2dcf82ad43eda
luizdivino <nandodivinodev@gmail.com>
2026-03-30 20:25:16 -0300

    feat: adicionar cor de fundo e tamanho do componente Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,7 +4,7 @@ interface CardProps {
 
 export const Card = ({children}: CardProps) => {
     return(
-        <div>
+        <div className="bg-gray-100 min-h-screen min-w-screen flex sm:px-4 ">
             {children}
         </div>
     )
