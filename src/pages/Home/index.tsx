7d50aceb9573e9944b007188d85a234910979864
luizdivino <nandodivinodev@gmail.com>
2026-03-30 16:57:18 -0300

    feat: renderizar o componente

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
+import { Card } from "../../components/Card"
+
 export const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <Card>
+                <h1>conteudo</h1>
+            </Card>
         </div>
     )
 }
