cc3d366e8d60cebbc5269b2f76e67a7235ac3332
luizdivino <nandodivinodev@gmail.com>
2026-03-30 17:52:23 -0300

    feat: receber dados da api usando useState e useEffect

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,32 @@
-import { Card } from "../../components/Card"
+import React from "react";
+import { useEffect, useState } from "react";
+import { Card } from "../../components/Card";
+import { api } from "../../services/api";
+import { ApiResponse } from "../../types/apiContract";
 
 export const Home = () => {
-    return (
-        <div>
-            <Card>
-                <h1>conteudo</h1>
-            </Card>
-        </div>
-    )
-}
+  const [data, setData] = useState<ApiResponse[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api();
+        setData(response);
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+
+  },[]);
+
+  return (
+    <div>
+      <Card>
+        <h1>conteudo</h1>
+      </Card>
+    </div>
+  );
+};
